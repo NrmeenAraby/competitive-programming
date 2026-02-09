a615c9ad7cba01cd0cde0ab38a44ba7dc3d1337f
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2026-02-09 02:02:49 +0200

    Add Car Factory.cpp for Codeforces Gym problem from 2017 ACM Amman Collegiate Programming Contest

---
+++ Codeforces/Gyms/Car Factory.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 1e9;
+const long double err = 1e-12;
+const long long MOD = 1e9 + 7;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin >> n >> k;
+        cout << n + k - 1 << endl;
+    }
+    return 0;
+}
