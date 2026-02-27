d3020a4f5cb618cce732093f195d80d67b302623
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2026-02-27 07:54:01 +0200

    Add Seats problem solution

---
+++ Codeforces/Div2/B/Seats.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 1e9;
+const long double err = 1e-12;
+const long long MOD = 1e9 + 7;
+int mul(int a, int b) {
+    return (1ll * a * b) % MOD;
+}
+vector<ll>fact(1e5 + 5);
+void buildFact() {
+    fact[0] = 1;
+    for (int i = 1; i <= 1e5; i++) {
+        fact[i] = mul(fact[i - 1], i);
+    }
+}
+ll binary_exp(ll base, ll p) {
+    ll ret = 1;
+    while (p) {
+        if (p % 2 != 0) {
+            ret = mul(ret, base);
+        }
+        base = mul(base, base);
+        p /= 2;
+    }
+    return ret;
+}
+int modInverse(int x) {
+    return binary_exp(x, MOD - 2);
+}
+int ncr(int n, int r) {
+    if (n < r)
+        return 0;
+    return mul(fact[n], modInverse(mul(fact[r], fact[n - r])));
+}
+bool isMnwara(int mask, int bit) {
+    return (mask >> bit) & 1;
+}
+vector<int> get_divisors(int n) {
+    vector<int>divs;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            divs.push_back(i);
+            if (n / i != i) {
+                divs.push_back(n/i);
+            }
+        }
+    }
+    return divs;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+       //100000001
+        //0000001
+        int n;
+        string s;
+        cin >> n>>s;
+     
+            int ans = 0;
+            int oneFlag = 0;
+           vector<int>oneidx;
+            for (int i = 0; i < n; i++) {
+                if (s[i] == '1') {
+                    oneFlag = 1;
+                    oneidx.push_back(i);
+                }
+            }
+            if (oneFlag == 0) {
+                ans = (n +2) / 3; 
+            }
+            else {
+                int bef = -1, after = -1;
+                int cntr = 0;
+                for (int i = 0; i < n; i++) {
+                    if (s[i] == '0') {
+                        cntr++;
+                    }
+                    else {
+                        ans++;
+                     
+                        if(bef == -1)
+                             bef = 1;
+                        else {
+                             after = 1;
+                        }
+                        
+    
+                            if (bef == 1 && after == 1) {
+                                cntr -= 2;
+                            }
+                            else {
+                                cntr--;
+                            }
+                            if (cntr == 1)
+                                ans++;
+                            else if (cntr > 0) {
+                                ans += ((cntr+2) / 3);
+                            }
+                            cntr = 0;    
+                    }
+                }
+                if (cntr-1 > 0) {
+                    cntr--;
+                    if (cntr == 1)
+                        ans++;
+                    else
+                     ans += ((cntr+2) / 3);
+                }
+
+            }
+            cout << ans << endl;
+        
+    }
+    return 0;
+}
