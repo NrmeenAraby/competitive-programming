642d0a69494dd00d937e4bbc2c9c3019595eb6c0
NrmeenAraby <swimmer.nrmeenaraby@gmail.com>
2026-07-12 19:03:25 +0300

    Add Sieve of Eratosthenes solution

---
+++ Codeforces/Div.3/A/Sieve of Erato67henes_2195A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 2e5 + 5;
+const int MOD = 1e9+7;
+
+ll add(ll a, ll b) {
+    return ((a % MOD) + (b % MOD)) % MOD;
+}
+ll sub(ll a, ll b) {
+    return ((a % MOD) - (b % MOD) + MOD) % MOD;
+}
+
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int x,flag67=0;
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            if (x == 67)flag67 = 1;
+        }
+        if (flag67)
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+    return 0;
+}
