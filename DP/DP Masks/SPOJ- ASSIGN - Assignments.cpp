d87f668e3eb8ca127f2fbf4d12b46a8177fa8a97
NrmeenAraby <swimmer.nrmeenaraby@gmail.com>
2026-07-21 00:47:55 +0300

    Add SPOJ ASSIGN problem solution in C++

---
+++ DP/DP Masks/SPOJ- ASSIGN - Assignments.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 3e5 + 5;
+const int MOD = 1e9+7;
+
+ll add(ll a, ll b) {
+    return ((a % MOD) + (b % MOD)) % MOD;
+}
+ll sub(ll a, ll b) {
+    return ((a % MOD) - (b % MOD) + MOD) % MOD;
+}
+vector<pair<int, int>>prime_factors;
+void factorize(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        int power = 0;
+        while (n % i == 0) {
+            power++;
+            n /= i;
+        }
+        if (power) {
+            prime_factors.push_back({ i,power });
+        }
+    }
+    if (n != 1) {
+        prime_factors.push_back({ n,1 });
+    }
+}
+//__builtin_popcount(mask)
+const ll N = (1ll << 21ll);
+int n;
+int studentTopic[22][22];
+ll dp[22][N];
+ll solve(int student = 0, ll topicsMask = 0) {
+    ;
+    if (student == n)
+        return 1;
+    if (dp[student][topicsMask] != -1)
+        return dp[student][topicsMask];
+
+    dp[student][topicsMask] = 0;
+    for (int topic = 0; topic < n; topic++) {
+        if (!((topicsMask >> topic) & 1)) {
+            if (studentTopic[student][topic]) {
+                dp[student][topicsMask] += solve(student + 1, topicsMask | (1ll << topic));
+            }
+        }
+    }
+    return  dp[student][topicsMask];
+}
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) {
+        cin >> n ;
+        //memset(dp, -1, sizeof dp);   
+        for (int i = 0; i <= 21; i++) {
+            for (int j = 0; j <= (1 << n); j++) {
+                dp[i][j] = -1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> studentTopic[i][j];
+            }
+        }
+        cout << solve() << endl;
+    }
+    return 0;
+}
