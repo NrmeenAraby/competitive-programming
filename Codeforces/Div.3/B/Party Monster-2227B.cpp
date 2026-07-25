1573baa7f89f5d94470e401aa80bbbc6a474862a
NrmeenAraby <swimmer.nrmeenaraby@gmail.com>
2026-07-26 00:33:56 +0300

    Add Party Monster solution for Codeforces 2227B

---
+++ Codeforces/Div.3/B/Party Monster-2227B.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 2e5 + 5;
+const int MOD = 1e9+7;
+
+ll add(ll a, ll b) {
+    return ((a % MOD) + (b % MOD)) % MOD;
+}
+ll sub(ll a, ll b) {
+    return ((a % MOD) - (b % MOD) + MOD) % MOD;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (n % 2) {
+            cout << "NO\n";
+        }
+        else {
+            int cntr = 0;
+            for (int i = 0; i < n; i++) {
+                if (s[i] == '(')
+                    cntr++;
+            }
+            if (cntr == n / 2) {
+                cout << "YES\n";
+            }
+            else {
+                cout << "NO\n";
+            }
+        }
+    }
+    return 0;
+}
