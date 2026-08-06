f5877c3aa5a021e9de144a18e8e650e87d68c312
NrmeenAraby <swimmer.nrmeenaraby@gmail.com>
2026-08-06 20:43:01 +0300

    Refactor Sparse Table implementation for clarity

--- Template/Sparse Table.cpp
+++ Template/Sparse Table.cpp
@@ -19,32 +19,40 @@
 #include <list>
 #define ll  long long
 using namespace std;
-vector<ll>lg;
 vector<vector<ll>>sp;
-void preLog(int n) {
-    lg.resize(n+1);
+vector<ll>lg;
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ 
+void preLog(int n) { //O(n)
+    lg.resize(n + 1);
     lg[1] = 0;
     for (int i = 2; i <= n; i++) {
         lg[i] = lg[i / 2] + 1;
     }
 }
-void buildSp(vector<int>&v) { //nlogn
+ll merge(ll a, ll b) {
+    return max(a, b);
+}
+void buildSP(vector<ll>& v) {  //nlogn
     int n = v.size();
-    sp.resize(n, vector<ll>(22));
+    sp.resize(n, vector<ll>(25, 0));
     for (int i = 0; i < n; i++) {
         sp[i][0] = v[i];
     }
     for (int pw = 1; (1 << pw) <= n; pw++) {
-        for (int i = 0; i+(1<<pw) <= n; i++) {
-            sp[i][pw] = min(sp[i][pw - 1], sp[i + (1 << (pw-1))][pw - 1]);
+        for (int i = 0; (i + (1 << pw)) <= n; i++) {
+            sp[i][pw] = merge(sp[i][pw - 1], sp[i + (1 << (pw - 1))][pw - 1]);
         }
     }
-
 }
-ll get(int l, int r) { //O(1)
+ll query(int l, int r) { //O(1)
     int sz = r - l + 1;
-    int pw = lg[sz]; //greatest power that gives length less than or equal r 
-    return min(sp[l][pw], sp[r - (1 << pw) + 1][pw]);
+    int pw = lg[sz];      //greatest power that gives length less than or equal r
+    return merge(sp[l][pw], sp[r - (1 << pw) + 1][pw]);
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -53,17 +61,17 @@ int main() {
 
     int n,q;
     cin >> n;
-    vector<int>v(n);
+    vector<ll>v(n);
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
     cin >> q;
     preLog(n);
-    buildSp(v);
+    buildSP(v);
     int l, r;
     while (q--) {
         cin >> l >> r;
-        cout << get(l, r)<<endl; // zero based
+        cout << query(l, r)<<endl; // zero based
     }
     
     return 0;
