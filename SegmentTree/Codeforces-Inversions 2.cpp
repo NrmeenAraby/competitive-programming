01800337c22f3718af2593abe67d219f14773ff8
NrmeenAraby <swimmer.nrmeenaraby@gmail.com>
2026-09-01 20:16:01 +0300

    Add Segment Tree implementation for inversion-2

---
+++ SegmentTree/Codeforces-Inversions 2.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <array>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <functional>
+#include <bitset>
+#include <cstring>
+#include <iomanip>
+#include <list>
+#define ll  long long
+using namespace std;
+const int MAX = 2e5 + 5;
+const int MOD = 1e9+7;
+struct SegmentTree {
+private:
+#define L 2*node+1   //to access the correct node value from the array of size 2n
+#define R 2*node+2
+#define mid (l+r)/2
+
+    int sz;
+    vector<ll>seg;
+    ll merge(ll a,ll b) {
+        return a + b;
+    }
+    void build(int l, int r, int node, vector<ll>& arr) {
+        if (l == r) {
+            if (l < arr.size()) {  // to avoid out of bound as the arr size is less than the seg size
+                seg[node] = 1;
+            }
+            return;
+        }
+        build(l, mid, L, arr);   //left
+        build(mid + 1, r, R, arr);  //right
+        seg[node] =merge(seg[L],seg[R]);
+    }
+    void update(int l, int r, int node, ll idx, ll val) {  //log n
+        if (l == r) {
+            seg[node]=val;
+            return;
+        }
+        if (idx <= mid) { //go left
+            update(l, mid, L, idx, val);
+        }
+        else { // go right
+            update(mid + 1, r, R, idx, val);
+        }
+        seg[node] = merge(seg[L] , seg[R]);
+    }
+
+    int find(int l, int r, int node, ll k) { // same as find k-th one problem, but this time give attention to right first 
+        if (l == r) {
+            return l;
+        }
+        if (seg[R] < k) { // so go left
+            return find(l, mid, L, k-seg[R]);
+        }
+        else {
+            return find(mid + 1, r, R, k );
+        }
+    }
+
+
+public:
+    SegmentTree(vector<ll>& arr) {
+        sz = 1;
+        int n = arr.size();
+        while (sz < n)sz *= 2; //to get first sz of power 2 greater than or equal n
+        seg.resize(2*sz);  //seg size is 2n
+        build(0, sz - 1, 0, arr);  //first call on the root node, whose range covers the entire array(0:sz-1)
+    }
+    void update(ll idx, ll val) {
+        update(0, sz - 1, 0, idx, val);
+    }
+    int find(ll k) {
+        return find(0, sz - 1, 0, k);
+    }
+
+
+};
+
+int main() {
+    // we start with a segment tree of zeros, we update the processed idx only with one, and at eah element we query a sum from elemnt+1 to n 
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    t = 1;
+    //cin >> t;
+    while (t--) {
+        int n;
+        cin >> n ;
+        vector<ll>a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        SegmentTree segT(a);
+        vector<ll>ans;
+        for (int i = n - 1; i >= 0;i--) {
+            ll ret = segT.find(a[i] + 1);
+            ans.push_back(ret+1);
+            segT.update(ret, 0ll); 
+        }
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            cout << ans[i] << " ";
+        }
+    }
+    return 0;
+} 
